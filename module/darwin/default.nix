8d206923f4d31719f62b9bc44e4df8c13d163e99
Samer Almasri <almasri@ualberta.ca>
2024-10-23 14:27:39 +0200

    refactori: custom user preferences
    move custom user preferences to file to separate file
    
    Signed-off-by: Samer Almasri <almasri@ualberta.ca>

--- module/darwin/default.nix
+++ module/darwin/default.nix
@@ -32,61 +32,7 @@
     # TODO control what shows in menu bar (top right)
     # TODO allow arc & zoom to share camera
     # TODO configure terminal profile
-    # TODO move the CustomUserPreferences to their own file
-    CustomUserPreferences = {
-      "com.apple.symbolichotkeys" = {
-        AppleSymbolicHotKeys = {
-          # Mission Control
-          "32" = {
-            enabled = 1;
-            value = {
-              parameters = [ 97 0 1572864 ];
-              type = "standard";
-            };
-          };
-          # Mission Control (probably not needed)
-          # "34" = {
-          #   enabled = 1;
-          #   value = {
-          #     parameters = [ 97 0 1703936 ];
-          #     type = "standard";
-          #   };
-          # };
-          # Move space left
-          "79" = {
-            enabled = true;
-            value = {
-              parameters = [ 122 6 1572864 ];
-              type = "standard";
-            };
-          };
-          # Move space left (probably not needed)
-          # "80" = {
-          #   enabled = 1;
-          #   value = {
-          #     parameters = [ 122 6 1703936 ];
-          #     type = "standard";
-          #   };
-          # };
-          # Move space right
-          "81" = {
-            enabled = true;
-            value = {
-              parameters = [ 120 7 1572864 ];
-              type = "standard";
-            };
-          };
-          # Move space right (probably not needed)
-          # "82" = {
-          #   enabled = true;
-          #   value = {
-          #     parameters = [ 120 7 1703936 ];
-          #     type = "standard";
-          #   };
-          # };
-        };
-      };
-    };
+    CustomUserPreferences = import ../settings/system-defaults.nix;
   };
 
   nix.extraOptions = ''
---
+++ module/settings/system-defaults.nix
@@ -0,0 +1,56 @@
+{
+  "com.apple.symbolichotkeys" = {
+    AppleSymbolicHotKeys = {
+      # Mission Control
+      "32" = {
+        enabled = 1;
+        value = {
+          parameters = [ 97 0 1572864 ];
+          type = "standard";
+        };
+      };
+
+      # Move space left
+      "79" = {
+        enabled = true;
+        value = {
+          parameters = [ 122 6 1572864 ];
+          type = "standard";
+        };
+      };
+
+      # Move space right
+      "81" = {
+        enabled = true;
+        value = {
+          parameters = [ 120 7 1572864 ];
+          type = "standard";
+        };
+      };
+      # Mission Control (probably not needed)
+      # "34" = {
+      #   enabled = 1;
+      #   value = {
+      #     parameters = [ 97 0 1703936 ];
+      #     type = "standard";
+      #   };
+      # };
+      # Move space left (probably not needed)
+      # "80" = {
+      #   enabled = 1;
+      #   value = {
+      #     parameters = [ 122 6 1703936 ];
+      #     type = "standard";
+      #   };
+      # };
+      # Move space right (probably not needed)
+      # "82" = {
+      #   enabled = true;
+      #   value = {
+      #     parameters = [ 120 7 1703936 ];
+      #     type = "standard";
+      #   };
+      # };
+    };
+  };
+}
