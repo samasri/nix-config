87873b0cc1700d09dc754540fd647dd588ad156f
Samer Almasri <almasri@ualberta.ca>
2024-10-25 00:16:21 +0200

    fix: specify custom key for github ssh access
    Signed-off-by: Samer Almasri <almasri@ualberta.ca>

--- module/settings/ssh-matchblocks.nix
+++ module/settings/ssh-matchblocks.nix
@@ -1,3 +1,8 @@
 {
-  # keeping this data private
+  "github.com" = {
+    hostname = "github.com";
+    user = "git";
+    identityFile = "/Users/m1/.ssh/ed25519";
+    identitiesOnly = true;
+  };
 }
